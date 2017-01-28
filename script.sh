d989096b4dcb0423cfc92a43b5298e55b9655f06
Prasoon Mehra <prasoonmehra14@gmail.com>
2017-01-29 00:11:04 +0530

    Added Script.sh for my convenience

---
+++ script.sh
@@ -0,0 +1,11 @@
+
+git clone https://github.com/phantom-1402/vendor_oneplus_oneplus3.git -b nougat vendor/oneplus
+git clone https://github.com/phantom-1402/android_device_oneplus_oneplus3 -b nougat device/oneplus/oneplus3
+git clone https://github.com/phantom-1402/android_device_oppo_common -b nougat device/oppo/common
+git clone https://github.com/LineageOS/android_device_qcom_common -b cm-14.1 device/qcom/common
+git clone https://github.com/LineageOS/android_kernel_oneplus_msm8996 -b cm-14.1 kernel/oneplus/msm8996
+git clone https://github.com/LineageOS/android_device_oneplus_common -b cm-14.1 device/oneplus/common
+git clone https://github.com/LineageOS/android_packages_resources_devicesettings -b cm-14.1 packages/resources/devicesettings
+git clone https://github.com/TheMuppets/proprietary_vendor_qcom_binaries -b cm-14.1 vendor/qcom/binaries
+
+
